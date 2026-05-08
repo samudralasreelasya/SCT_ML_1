d0df612a00402a25a511f2a9193e9955e6d1e48b
samudralasreelasya <samudralasreelasya@gmail.com>
2026-05-08 20:28:56 +0530

    Completed Task 1 - House Price Prediction Project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# SCT_ML_1
+# SCT_ML_1# SCT_ML_1 - House Price Prediction
+
+## 📌 Task Objective
+Build a Linear Regression model to predict house prices based on:
+- Square footage (GrLivArea)
+- Number of bedrooms (BedroomAbvGr)
+- Number of bathrooms (FullBath)
+
+## 📊 Dataset
+Housing dataset containing multiple features.  
+For this task, only selected features were used.
+
+## ⚙️ Technologies Used
+- Python
+- Pandas
+- Scikit-learn
+
+## 🧠 Model Used
+Linear Regression
+
+## 🚀 How to Run
+1. Clone the repository
+2. Install dependencies:
+   pip install -r requirements.txt
+3. Run:
+   python src/model.py
+
+## 📈 Output
+- Predicts house prices
+- Displays Mean Squared Error (MSE)
+- Shows feature coefficients
+
+## 📂 Project Structure
+SCT_ML_1/
+ ┣ data/
+ ┃ ┣ train.csv
+ ┣ src/
+ ┃ ┣ model.py
+ ┣ README.md
+ ┣ requirements.txt
--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 # Load dataset
 df = pd.read_csv("data/train.csv")
@@ -31,3 +32,22 @@ mse = mean_squared_error(y_test, predictions)
 
 print("Mean Squared Error:", mse)
 print("Coefficients:", model.coef_)
+# bar plot for Neighborhood distribution
+plt.figure(figsize=(12,5))
+
+df['Neighborhood'].value_counts().plot(kind='bar')
+
+plt.xlabel("Neighborhood")
+plt.ylabel("Number of Houses")
+plt.title("Houses in Each Neighborhood")
+plt.show()
+#scatter plot for GrLivArea vs SalePrice
+plt.figure(figsize=(8,5))
+
+plt.scatter(df['GrLivArea'], df['SalePrice'])
+
+plt.xlabel("Ground Living Area")
+plt.ylabel("Sale Price")
+plt.title("Living Area vs Sale Price")
+
+plt.show()
